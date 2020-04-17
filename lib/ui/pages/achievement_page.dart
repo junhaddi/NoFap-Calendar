5c1c6a52b781ab3ab4a3c049ecb2fe287bc9f966
junhaddi <rkdwnsgk05@gmail.com>
2020-04-17 16:44:53 +0900

    업적 페이지 개선

--- lib/ui/pages/achievement_page.dart
+++ lib/ui/pages/achievement_page.dart
@@ -10,14 +10,30 @@ class AchievementPage extends StatefulWidget {
 class _AchievementPageState extends State<AchievementPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Icon(Icons.cake),
-          Text('업적'),
-        ],
-      ),
+    return GridView.count(
+      crossAxisCount: 2,
+      children: List.generate(100, (index) {
+        return Card(
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Image.network(
+                  'https://pds.joins.com/news/component/htmlphoto_mmdata/201906/27/htm_20190627161112785240.jpg'),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('금딸의 신'),
+                    SizedBox(height: 8.0),
+                    Text('Index $index'),
+                  ],
+                ),
+              )
+            ],
+          ),
+        );
+      }),
     );
   }
 }
